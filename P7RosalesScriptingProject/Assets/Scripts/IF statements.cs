a77d81d580676ab014f57899d5aac763a43baa89
733482-code <733482@student.dusd.net>
2025-11-12 14:24:22 -0800

    scripting work
    finished code but need to fix for play mode

--- P7RosalesScriptingProject/Assets/Scripts/IF statements.cs
+++ P7RosalesScriptingProject/Assets/Scripts/IF statements.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class IFstatements : MonoBehaviour
@@ -26,4 +27,19 @@ void TempertureTest ()
 {
     // If the coffee's temperture is greater yjan the hottest drinking temperture...
     if (coffeeTemperture > hotLimitTemperture)
-}
+    {
+        // ... do this.
+        print("coffee is Too hot.");
+    }
+    // if isn't, but the coffee temperture is less than the coldest drinking temerpture...
+    else if(coffeeTemperture < coldLimitTemperture)
+    {
+        // ... do this.
+        print("Coffee is too cold");
+        )
+        // if it is neither of those then...
+        else
+        { 
+            // ... do this.
+            fsJsonPrinter()
+
